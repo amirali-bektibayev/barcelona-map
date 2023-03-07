f598cdeacc6edc6c6b08e9f6d434142eafb9d9e8
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-03-07 13:18:18 +0100

    Pagination

--- src/components/Panel/Panel.jsx
+++ src/components/Panel/Panel.jsx
@@ -3,13 +3,26 @@ import "./Panel.style.css";
 import { ALL_POINTS } from "../../data/points/all_points";
 import PointsList from "../PointsList/PointsList";
 import PanelFilter from "../PanelFilter/PanelFilter";
+import usePagination from "../../hooks/usePagination";
+import Pagination from "@mui/material/Pagination";
 
 const Panel = ({ getPointId }) => {
   const [filteredPoints, setFilteredPoints] = useState(ALL_POINTS);
+  const {
+    nextPage,
+    prevPage,
+    setPage,
+    firstContentIndex,
+    lastContentIndex,
+    page,
+    totalPages,
+  } = usePagination({ contentPerPage: 5, count: filteredPoints.length });
 
-  const p = filteredPoints.map((p) => {
-    return <PointsList getPointId={getPointId} point={p} />;
-  });
+  const p = filteredPoints
+    .slice(firstContentIndex, lastContentIndex)
+    .map((p) => {
+      return <PointsList getPointId={getPointId} point={p} />;
+    });
 
   //   const allPoints = ALL_POINTS.map((point) => {
   //     return <PointsList point={point} />;
@@ -17,12 +30,17 @@ const Panel = ({ getPointId }) => {
 
   const filterHandle = (value) => {
     const filtered = ALL_POINTS.filter((point) => {
+      setPage(1);
       if (value === "all") {
         return point;
       }
       return point.type === value;
     });
     setFilteredPoints(filtered);
+  };
+
+  const handlePageChange = (e, value) => {
+    setPage(value);
   };
 
   return (
@@ -32,6 +50,10 @@ const Panel = ({ getPointId }) => {
       <div className="panel-list-wrapper">
         <div className="panel-list-title">List</div>
         <div className="panel-list">{p}</div>
+
+        <div className="panel-list-pagination">
+          <Pagination onChange={handlePageChange} count={totalPages} />
+        </div>
       </div>
     </div>
   );
