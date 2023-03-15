c479b6be567b65fe4bf6d7ce47488c4d9899cd57
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-03-15 21:23:00 +0100

    some changes

--- src/components/HoverPoint/HoverPoint.jsx
+++ src/components/HoverPoint/HoverPoint.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import {
-  xFirstHovCon,
-  xSecondHovCon,
-  yFirstHovCon,
-  ySecondHovCon,
-} from "./HoverPoint.helpers";
+import {} from "./HoverPoint.helpers";
 import "./HoverPoint.style.css";
 
 const HoverPoint = ({ point }) => {
+  const block_40_41 = (point_id_adress) => {
+    if (point_id_adress == 40) {
+      return "block_40";
+    }
+    if (point_id_adress == 41) {
+      return "block_41";
+    }
+  };
+
   return (
-    <div
-      className={`hover ${xFirstHovCon(point.block_address)} ${xSecondHovCon(
-        point.block_address
-      )} ${yFirstHovCon(point.block_address)} ${ySecondHovCon(
-        point.block_address
-      )}`}
-    >
+    <div className={`hover ${block_40_41(point.id_address)} `}>
       <div className="hover-point-item hover-name">
         <span>name:</span> "{point.name}"
       </div>
