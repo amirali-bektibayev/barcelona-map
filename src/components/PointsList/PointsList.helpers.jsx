c352ab2a6cb0bb38a2f396052dfa591f1d5934f3
Amir <102328593+amirali-bektibayev@users.noreply.github.com>
2023-03-16 04:51:53 +0100

    new images

--- src/components/PointsList/PointsList.helpers.jsx
+++ src/components/PointsList/PointsList.helpers.jsx
@@ -4,6 +4,11 @@ import TapasImg from "../../assets/img/tapas.jpg";
 import PizzaImg from "../../assets/img/pizza.jpg";
 import BurgerImg from "../../assets/img/burger.jpg";
 import ShopImg from "../../assets/img/shop.jpg";
+import PharmacyImg from "../../assets/img/pharmacy.jpg";
+import SeaFoodImg from "../../assets/img/sea-food.jpg";
+import DessertImg from "../../assets/img/dessert.jpg";
+import ParkingImg from "../../assets/img/parking.jpg";
+import FastFoodImg from "../../assets/img/fast-food.jpg";
 
 export const PointTypeImage = ({ point }) => {
   if (point.type == "bar") {
@@ -45,6 +50,41 @@ export const PointTypeImage = ({ point }) => {
     return (
       <div>
         <img className="point-type-image" src={TapasImg} />
+      </div>
+    );
+  }
+  if (point.type == "sea") {
+    return (
+      <div>
+        <img className="point-type-image" src={SeaFoodImg} />
+      </div>
+    );
+  }
+  if (point.type == "pharmacy") {
+    return (
+      <div>
+        <img className="point-type-image" src={PharmacyImg} />
+      </div>
+    );
+  }
+  if (point.type == "dessert") {
+    return (
+      <div>
+        <img className="point-type-image" src={DessertImg} />
+      </div>
+    );
+  }
+  if (point.type == "parking") {
+    return (
+      <div>
+        <img className="point-type-image" src={ParkingImg} />
+      </div>
+    );
+  }
+  if (point.type == "fast-food") {
+    return (
+      <div>
+        <img className="point-type-image" src={FastFoodImg} />
       </div>
     );
   }
